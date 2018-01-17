e2bb551a16190bce2c3ed181468baeb407ce6e09
Krishna Bagal <krishna.bagal@gmail.com>
2018-01-17 10:27:21 +0530

    Updated command to delete last year files and folders.
    Updated command to delete last year files and folders

--- syslogs.sh
+++ syslogs.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==#
 #Script Name: syslogs.sh 
 #Date: Sep 08th, 2017. 
-#Modified: NA
-#Versioning: NA
-#Owner: Krishna Bagal.
+#Modified: Jan 11th, 2017.
+#Versioning: 0.1 = updated command to delete old files (last 30 days) folder from
+#                  location. (Dec 19th, 2017) 
+#            0.2 = Updated command to delete last year files and folders. (Jan 11th, 2017.)
+#Author: Krishna Bagal.
 #Info: Script will dump ps, free, top and netstat logs for reference.
-#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
-
+#Ticket: NA
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==#
 DATEnTIME=`date +%Y_%m_%d-%H:%M`
 DATE=`date +%Y_%m_%d`
 LOGDIR="/var/log/systemlogs"
 MKDIR="/bin/mkdir"
-
+CURMONTH=`date +%Y_%m_`
+ONEMONTH=`date +%Y_%m_30 -d "1 month ago"`
+LASTMONTH=`date +%m -d "1 month ago"`
+LASTYEAR=`date +%Y -d "last year"`
+DELCURFOLDER=`date +%Y_"$LASTMONTH"_??`
+DELLASTFOLDER=`date +"$LASTYEAR"_"$LASTMONTH"_??`
 if [ -d $LOGDIR ];
 then
         if [ -d $LOGDIR/$DATE ];
@@ -25,7 +32,7 @@ then
                 echo "$LOGDIR/$DATE Directory not present. Creating now...!!!"
                 sleep 3
                 $MKDIR -p $LOGDIR/$DATE
-                /bin/ps auxfff > $LOGDIR/$DATE/$DATEnTIME-ps
+                /bin/ps -auxfff > $LOGDIR/$DATE/$DATEnTIME-ps
                 /usr/bin/top -c -n1 -b > $LOGDIR/$DATE/$DATEnTIME-top
                 /usr/bin/free -mt > $LOGDIR/$DATE/$DATEnTIME-free
                 /bin/netstat -atunlp > $LOGDIR/$DATE/$DATEnTIME-netstat
@@ -35,4 +42,12 @@ else
         echo "Creating now.!!!!"
         $MKDIR -p $LOGDIR
 fi
-/usr/bin/find /var/log/systemlogs/ -mtime +30 -exec rm -rf {} \;
+ 
+if [ -d $LOGDIR/$ONEMONTH ];
+then
+        rm -rfv $LOGDIR/$DELCURFOLDER
+        rm -rfv $LOGDIR/$DELLASTFOLDER
+else 
+        echo "This Months Logs are Present."
+        exit 0
+fi
