208c1e568e94a0da77c5ab3b99054b7f9a4a715b
Krishna Bagal <krishna.bagal@gmail.com>
2018-03-26 13:21:04 +0530

    Update data/logs in single file.

--- syslogs.sh
+++ syslogs.sh
@@ -2,10 +2,12 @@
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==#
 #Script Name: syslogs.sh 
 #Date: Sep 08th, 2017. 
-#Modified: Jan 11th, 2017.
+#Modified: Mar 20th, 2018.
 #Versioning: 0.1 = updated command to delete old files (last 30 days) folder from
 #                  location. (Dec 19th, 2017) 
 #            0.2 = Updated command to delete last year files and folders. (Jan 11th, 2017.)
+#            0.3 = Updated command to keep only one week logs. (Mar 19th, 2018.)
+#            0.3 = Update data/logs in single file. (Mar 20th, 2018.)
 #Author: Krishna Bagal.
 #Info: Script will dump ps, free, top and netstat logs for reference.
 #Ticket: NA
@@ -15,27 +17,60 @@ DATE=`date +%Y_%m_%d`
 LOGDIR="/var/log/systemlogs"
 MKDIR="/bin/mkdir"
 CURMONTH=`date +%Y_%m_`
-ONEMONTH=`date +%Y_%m_30 -d "1 month ago"`
-LASTMONTH=`date +%m -d "1 month ago"`
-LASTYEAR=`date +%Y -d "last year"`
-DELCURFOLDER=`date +%Y_"$LASTMONTH"_??`
-DELLASTFOLDER=`date +"$LASTYEAR"_"$LASTMONTH"_??`
+LASTWEEK=`date +%d -d "1 week ago"`
+HOSTNAME=`hostname -f`
+HOSTIP=`hostname -i`
+UPTIME=`/usr/bin/uptime`
+CURRENTDATE=`date`
+KERNELVERSION=`/bin/uname -r`
 if [ -d $LOGDIR ];
 then
         if [ -d $LOGDIR/$DATE ];
         then
-                /bin/ps auxfff > $LOGDIR/$DATE/$DATEnTIME-ps
-                /usr/bin/top -c -n1 -b > $LOGDIR/$DATE/$DATEnTIME-top
-                /usr/bin/free -mt > $LOGDIR/$DATE/$DATEnTIME-free
-                /bin/netstat -atunlp > $LOGDIR/$DATE/$DATEnTIME-netstat
+                echo "==============================: System Info :==============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo -e "\t\t\t\t\t\t$CURRENTDATE" >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "Hostname: $HOSTNAME" >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "Host IP: $HOSTIP" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /usr/bin/lsb_release -d >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "Kernel Version: $KERNELVERSION" >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "" >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo Uptime: $UPTIME >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "" >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "=============================: Space Log  :===============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /bin/df -hT >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "=============================: Memory Info :==============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /usr/bin/free -mlt >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "=============================: Process Log :==============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /bin/ps -eo %mem,%cpu,user,pid,ppid,stat,start,cmd --sort=-%mem >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "=============================: Top Command :==============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /usr/bin/top -c -n1 -b >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "=============================: Network Log :==============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /bin/netstat -atunlp >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "==========================================================================" >> $LOGDIR/$DATE/$DATEnTIME.log
         else 
                 echo "$LOGDIR/$DATE Directory not present. Creating now...!!!"
                 sleep 3
                 $MKDIR -p $LOGDIR/$DATE
-                /bin/ps -auxfff > $LOGDIR/$DATE/$DATEnTIME-ps
-                /usr/bin/top -c -n1 -b > $LOGDIR/$DATE/$DATEnTIME-top
-                /usr/bin/free -mt > $LOGDIR/$DATE/$DATEnTIME-free
-                /bin/netstat -atunlp > $LOGDIR/$DATE/$DATEnTIME-netstat
+                echo "==============================: System Info :==============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo -e "\t\t\t\t\t\t\t\t$date"
+                echo "Hostname: $HOSTNAME" >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "Host IP: $HOSTIP" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /usr/bin/lsb_release -d >> $LOGDIR/$DATE/$DATEnTIME.log
+                /bin/uname -r >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "" >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo $UPTIME >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "" >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "=============================: Space Log  :===============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /bin/df -hT >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "=============================: Memory Info :==============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /usr/bin/free -mlt >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "=============================: Process Log :==============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /bin/ps -eo %mem,%cpu,user,pid,ppid,stat,start,cmd --sort=-%mem >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "=============================: Top Command :==============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /usr/bin/top -c -n1 -b >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "=============================: Network Log :==============================" >> $LOGDIR/$DATE/$DATEnTIME.log
+                /bin/netstat -atunlp >> $LOGDIR/$DATE/$DATEnTIME.log
+                echo "==========================================================================" >> $LOGDIR/$DATE/$DATEnTIME.log
         fi
 else 
         echo "$LOGDIR Directory is not present."
@@ -43,11 +78,4 @@ else
         $MKDIR -p $LOGDIR
 fi
  
-if [ -d $LOGDIR/$ONEMONTH ];
-then
-        rm -rfv $LOGDIR/$DELCURFOLDER
-        rm -rfv $LOGDIR/$DELLASTFOLDER
-else 
-        echo "This Months Logs are Present."
-        exit 0
-fi
+/usr/bin/find $LOGDIR -type d -mtime +7 -exec rm -rfv {} \;
